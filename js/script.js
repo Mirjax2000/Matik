3fdfeeaa0b849c7fcd43e22d210b1fb54f9b3caa
Vigo <miroslav.viktorin@seznam.cz>
2024-05-22 21:52:40 +0200

    factorial

--- js/script.js
+++ js/script.js
@@ -5,6 +5,35 @@
 // -----------------
 
 $(function () {
-  
-  
+    let theme = $('.theme'),
+        body = $('body'),
+        theme_switch = $('.switch'),
+        main = $('.main'),
+        menu = $('.menu'),
+        ajax = $('#ajax'),
+        content = $('#content'),
+        faktorialContent = $('#faktorial__content');
+
+    // Theme
+    toggle = function () {
+        body.toggleClass('dark');
+        theme.toggleClass('off');
+    };
+    localStorage.getItem('theme') === 'dark' ? toggle() : null;
+    theme_switch.on('click', function () {
+        toggle();
+        localStorage.getItem('theme') === 'dark'
+            ? localStorage.clear()
+            : localStorage.setItem('theme', 'dark');
+    });
+    // ----------------------------------------------
+
+    // menu ajax
+    menu.on('click', 'li a', function (event) {
+        event.preventDefault();
+        let link = $(this).attr('href');
+        content.hide();
+        ajax.load(link + ' #faktorial__content');
+    });
+    // -----------------------------------------------
 });
